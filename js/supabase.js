1dabd98263c12cc28372822b58ca2d7db0f99c7f
Underthestairs95 <desleykramerprive@outlook.com>
2026-08-02 17:13:30 +0200

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -2,12 +2,11 @@ const SUPABASE_URL = "https://wozdvncbghqrafxhpcjy.supabase.co";
 
 const SUPABASE_KEY = "sb_publishable_0xnm_ZnN4OUMby7hSHOrNA_gI0xlKdR";
 
-const supabaseClient = window.supabase.createClient(
+const supabaseClient = supabase.createClient(
     SUPABASE_URL,
     SUPABASE_KEY
 );
 
-// Globaal beschikbaar maken
 window.supabaseClient = supabaseClient;
 
 console.log("✅ Verbonden met Supabase!");
